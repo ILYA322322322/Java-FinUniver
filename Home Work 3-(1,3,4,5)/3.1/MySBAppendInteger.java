469938e3569edfe63d4e19858645bb6bcdf3a804
Илья Сафронов <bizzon20101010@gmail.com>
2022-11-05 06:19:45 +0300

    3.1
    3.1

---
+++ Home Work 3-(1,3,4,5)/3.1/MySBAppendInteger.java
@@ -0,0 +1,9 @@
+public class MySBAppendInteger extends MySB{
+    public MySBAppendInteger(StringBuilder sb) {
+        super(sb);
+    }
+
+    public void append(int i) {
+        this.sb.append(i);
+    }
+}
